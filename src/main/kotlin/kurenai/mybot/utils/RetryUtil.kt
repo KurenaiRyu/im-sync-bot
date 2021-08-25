25dd5b85aa9267d2ecd165b270cb9518c6d3a73d
Kurenai <kurenai233@yahoo.com>
2021-08-25 10:06:48 +0800

    pref: 优化重试工具

--- src/main/kotlin/kurenai/mybot/utils/RetryUtil.kt
+++ src/main/kotlin/kurenai/mybot/utils/RetryUtil.kt
@@ -1,26 +1,13 @@
 package kurenai.mybot.utils
 
-import kotlinx.atomicfu.locks.withLock
-import kurenai.mybot.cache.DelayItem
+import kotlinx.coroutines.delay
 import mu.KotlinLogging
-import java.util.concurrent.DelayQueue
-import java.util.concurrent.TimeUnit
-import java.util.concurrent.locks.Condition
-import java.util.concurrent.locks.ReentrantLock
 
 private val log = KotlinLogging.logger {}
 
 object RetryUtil {
 
-    init {
-        val daemonThread = Thread { retryDaemon() }
-        daemonThread.isDaemon = true
-        daemonThread.name = "Retry Daemon"
-        daemonThread.start()
-    }
-
     private const val MAX_TIMES = 3
-    private val delayQueue = DelayQueue<DelayItem<Condition>>()
 
     @Throws(Exception::class)
     suspend fun <T> aware(callable: SuspendCallable<T>, consumer: SuspendConsumer<T?, Throwable?>) {
@@ -28,10 +15,7 @@ object RetryUtil {
             val result = callable.call()
             consumer.accept(result, null)
         } catch (e: Exception) {
-            val lock = ReentrantLock()
-            val cond = lock.newCondition()
-            delayQueue.add(DelayItem(cond, getDelayTime(1)))
-            doRetry(callable, consumer, lock, cond)
+            doRetry(callable, consumer)
             log.error(e.message, e)
         }
     }
@@ -39,40 +23,24 @@ object RetryUtil {
     private suspend fun <T> doRetry(
         callable: SuspendCallable<T>,
         consumer: SuspendConsumer<T?, Throwable?>,
-        lock: ReentrantLock,
-        cond: Condition,
-        count: Int = 1
     ) {
-        lock.withLock {
+        var count = 1
+        var isDone = false
+        while (!isDone)
             try {
-                cond.await(getDelayTime(MAX_TIMES) + 5000L, TimeUnit.MILLISECONDS)
+                delay(getDelayTime(count))
                 val result = callable.call()
                 consumer.accept(result, null)
+                isDone = true
             } catch (e: Exception) {
-                log.error("Retry fail $count time(s).", e)
-                val nextCount = count + 1
-                if (MAX_TIMES >= nextCount) {
-                    delayQueue.add(DelayItem(cond, getDelayTime(nextCount)))
-                    doRetry(callable, consumer, lock, cond, nextCount)
-                } else {
+                log.error("Retry fail $count times. ${e.message}")
+                count++
+                if (MAX_TIMES < count) {
                     log.error("Retry over $MAX_TIMES times. Discard.")
                     consumer.accept(null, e)
+                    isDone = true
                 }
             }
-        }
-    }
-
-    private fun retryDaemon() {
-        log.info("Retry daemon started.")
-        while (true) {
-            try {
-                delayQueue.take().item.signal()
-            } catch (e: InterruptedException) {
-                log.error("Retry daemon run fail.", e)
-                break
-            }
-        }
-        log.info("Retry daemon stopped.")
     }
 
     private fun getDelayTime(times: Int): Long {
