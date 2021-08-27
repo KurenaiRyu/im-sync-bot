c0553816910e0234a35c9307a750a774631f986c
Kurenai <kurenai233@yahoo.com>
2021-08-27 13:23:58 +0800

    feat: 增加定时清除缓存文件功能

--- src/main/kotlin/kurenai/mybot/config/BotInitializer.kt
+++ src/main/kotlin/kurenai/mybot/config/BotInitializer.kt
@@ -3,11 +3,20 @@ package kurenai.mybot.config
 import kurenai.mybot.BotConfigConstant
 import kurenai.mybot.ContextHolder
 import kurenai.mybot.repository.BotConfigRepository
+import mu.KotlinLogging
+import org.apache.commons.io.FileUtils
+import org.apache.commons.io.filefilter.AgeFileFilter
+import org.apache.commons.lang3.time.DateUtils
 import org.springframework.beans.factory.InitializingBean
+import java.io.File
+import java.util.*
+import kotlin.concurrent.timerTask
 
 class BotInitializer(
-    private val botConfigRepository: BotConfigRepository
+    private val botConfigRepository: BotConfigRepository,
 ) : InitializingBean {
+
+    private val log = KotlinLogging.logger {}
 
     override fun afterPropertiesSet() {
         if (botConfigRepository.existsById(BotConfigConstant.MASTER_CHAT_ID)) {
@@ -15,5 +24,28 @@ class BotInitializer(
                 ContextHolder.masterChatId = it.toLong()
             }
         }
+
+        val clearCacheTimer = Timer("clear cache file", true)
+        clearCacheTimer.scheduleAtFixedRate(timerTask {
+            val oldestAllowedFileDate = DateUtils.addDays(Date(), -3) //minus days from current date
+            val cacheDir = File("./cache")
+
+            val filesToDelete = ArrayList<File>()
+            cacheDir.listFiles()?.forEach { file ->
+                if (file.isDirectory) {
+                    filesToDelete.addAll(FileUtils.listFiles(file, AgeFileFilter(oldestAllowedFileDate), null))
+                }
+            }
+
+            if (filesToDelete.isNotEmpty()) {
+                //if deleting subdirs, replace null above with TrueFileFilter.INSTANCE
+                log.debug { "Clearing cache files..." }
+                filesToDelete.forEach {
+                    log.debug { "${it.name} deleted." }
+                    FileUtils.deleteQuietly(it)
+                } //I don't want an exception if a file is not deleted. Otherwise use filesToDelete.next().delete() in a try/catch
+                log.debug { "Clear ${filesToDelete.size} cache files." }
+            }
+        }, 5000L, 24 * 60 * 60 * 1000L)
     }
 }
