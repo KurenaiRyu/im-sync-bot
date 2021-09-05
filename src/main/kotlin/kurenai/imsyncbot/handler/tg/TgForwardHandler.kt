a179e1750a9afcfe81fe2d43b756832702eeee74
Kurenai <kurenai233@yahoo.com>
2021-09-05 19:42:26 +0800

    perf: tg频道以及tg官方id不加前缀

--- src/main/kotlin/kurenai/imsyncbot/handler/tg/TgForwardHandler.kt
+++ src/main/kotlin/kurenai/imsyncbot/handler/tg/TgForwardHandler.kt
@@ -239,6 +239,8 @@ class TgForwardHandler(
         val from = message.from
         if (from.userName.equals("GroupAnonymousBot", true)) {
             return message.authorSignature ?: ""    // 匿名用头衔作为前缀，空头衔将会不添加前缀
+        } else if (message.isChannelMessage || from.id == 777000L) { //tg 频道以及tg官方id不加前缀
+            return ""
         } else {
             return bindingName[from.id] ?: let {
                 val username = "${from.firstName} ${from.lastName ?: ""}"
