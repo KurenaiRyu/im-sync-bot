34dbe6dd3ed45bce7fc9745925e03f515c9346a0
Kurenai <kurenai233@yahoo.com>
2021-12-17 12:56:15 +0800

    fix: coroutine stop when throw exception

--- src/main/kotlin/kurenai/imsyncbot/qq/QQBotClient.kt
+++ src/main/kotlin/kurenai/imsyncbot/qq/QQBotClient.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.*
 import kurenai.imsyncbot.BotConfigKey
 import kurenai.imsyncbot.ContextHolder
 import kurenai.imsyncbot.HandlerHolder
+import kurenai.imsyncbot.ImSyncBotRuntimeException
 import kurenai.imsyncbot.config.BotProperties
 import kurenai.imsyncbot.handler.Handler.Companion.CONTINUE
 import kurenai.imsyncbot.handler.Handler.Companion.END
@@ -25,6 +26,7 @@ import org.springframework.stereotype.Component
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import java.util.concurrent.LinkedBlockingDeque
 import kotlin.system.exitProcess
+import kotlin.system.measureTimeMillis
 
 @Component
 class QQBotClient(
@@ -83,13 +85,6 @@ class QQBotClient(
             }
 
             filter.subscribeAlways<Event> { event ->
-                if (eventDeque.remainingCapacity() <= 0) {
-                    synchronized(eventDeque) {
-                        if (eventDeque.remainingCapacity() <= 0) {
-                            runBlocking { delay(500) }
-                        }
-                    }
-                }
                 try {
                     eventDeque.push(event)
                 } catch (e: IllegalStateException) {
@@ -97,30 +92,42 @@ class QQBotClient(
                 }
             }
             val handlerScope = CoroutineScope(Dispatchers.IO)
+            var count = 0
             handlerScope.launch {
                 while (isActive) {
-                    try {
-                        when (val event = eventDeque.take()) {
-                            is FriendEvent -> {
-                                privateChatHandler.onFriendEvent(event)
+                    count = count % 10000 + 1
+                    val c = count
+                    log.debug { "${Thread.currentThread().name}-$c take queue" }
+                    val event = eventDeque.take()
+                    log.debug { "${Thread.currentThread().name}-$c queue: $event" }
+                    handlerScope.launch {
+                        try {
+                            measureTimeMillis {
+                                when (event) {
+                                    is FriendEvent -> {
+                                        privateChatHandler.onFriendEvent(event)
+                                    }
+                                    is MessageEvent -> {
+                                        handle(event)
+                                    }
+                                    is MessageRecallEvent.GroupRecall -> {
+                                        forwardHandler.onRecall(event)
+                                    }
+                                    is GroupEvent -> {
+                                        forwardHandler.onGroupEvent(event)
+                                    }
+                                    else -> {
+                                        log.trace { "未支持事件 ${event.javaClass} 的处理" }
+                                    }
+                                }
+                            }.let {
+                                log.debug { "${Thread.currentThread().name}-$c ${it}ms" }
                             }
-                            is MessageEvent -> {
-                                handle(event)
-                            }
-                            is MessageRecallEvent.GroupRecall -> {
-                                forwardHandler.onRecall(event)
-                            }
-                            is GroupEvent -> {
-                                forwardHandler.onGroupEvent(event)
-                            }
-                            else -> {
-                                log.trace { "未支持事件 ${event.javaClass} 的处理" }
-                            }
+                        } catch (e: CancellationException) {
+                            log.error(e) { "Coroutine was canceled: ${e.message}" }
+                        } catch (e: Exception) {
+                            log.error(e) { e.message }
                         }
-                    } catch (e: CancellationException) {
-                        log.error(e) { "Coroutine was canceled: ${e.message}" }
-                    } catch (e: Exception) {
-                        log.error(e) { e.message }
                     }
                 }
                 log.error { "QQ handler scope is inactive." }
@@ -141,6 +148,8 @@ class QQBotClient(
                 }
                 if (result == END) break
             }
+        } catch (e: ImSyncBotRuntimeException) {
+            log.warn { e.message }
         } catch (e: Exception) {
             log.error(e) { "处理信息失败，发送失败报告。" }
             try {
