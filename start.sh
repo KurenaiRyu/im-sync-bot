fc18e1f96ae08b6356ad4b549b6c44a285c90d29
Kurenai <kurenai233@yahoo.com>
2024-09-04 05:53:38 +0800

    chore(scrip): add utf-8 encoding config

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xms150m -Xmx300m -jar im-sync-bot.jar
+java -Xms150m -Xmx300m -Dfile.encoding=UTF-8 -Dstdout.encoding=UTF-8 -Dstderr.encoding=UTF-8 -Dconsole.encoding=UTF-8 -jar im-sync-bot.jar
